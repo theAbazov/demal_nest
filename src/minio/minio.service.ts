7ae69f0ccc676491cc1acadec9d93f8b633ef6fc
Duu1at <dbolsunbekuulu@gmail.com>
2026-01-17 01:58:41 +0600

    fix MinioService

--- src/minio/minio.service.ts
+++ src/minio/minio.service.ts
@@ -8,16 +8,16 @@ export class MinioService implements OnModuleInit {
   private bucketName: string;
 
   constructor(private readonly configService: ConfigService) {
-    this.bucketName = this.configService.get<string>('MINIO_BUCKET_NAME') || 'uploads';
+    this.bucketName = this.configService.get<string>('MINIO_BUCKET_NAME');
   }
 
   onModuleInit() {
     this.minioClient = new Minio.Client({
-      endPoint: this.configService.get<string>('MINIO_ENDPOINT') || '109.73.202.55',
-      port: parseInt(this.configService.get<string>('MINIO_PORT') || '9000'),
+      endPoint: this.configService.get<string>('MINIO_ENDPOINT'),
+      port: parseInt(this.configService.get<string>('MINIO_PORT')),
       useSSL: this.configService.get<string>('MINIO_USE_SSL') === 'true',
-      accessKey: this.configService.get<string>('MINIO_ACCESS_KEY') || 'your_access_key01',
-      secretKey: this.configService.get<string>('MINIO_SECRET_KEY') || 'your_secret_key01',
+      accessKey: this.configService.get<string>('MINIO_ACCESS_KEY'),
+      secretKey: this.configService.get<string>('MINIO_SECRET_KEY'),
     });
   }
 
